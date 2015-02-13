07b29943b66fcbeee9c33beea3630131ec4ee8a4
PromiscuousFish <jacob.bisnett@gmail.com>
2015-02-13 17:48:45 -0500

    Update submitForm.js

--- javascripts/submitForm.js
+++ javascripts/submitForm.js
@@ -1,17 +1,30 @@
 Parse.initialize("DTlgg5qm0fyJxShtYY2RzjX62KgO7jcx4Y3fUV8S", "GYABuByO1Kg8wx8isoHAKPixxzpJLlLtDkx37zhE");
-var TestObject = Parse.Object.extend("TestObject");
-var testObject = new TestObject();
+var Users = Parse.Object.extend("users");
+var user = new Users();
 
 
-testObject.save({foo: "bar"}).then(function(object) {
-});
+
+//testObject.save({foo: "bar"}).then(function(object) {
+//});
 
 window.onload = function() {
     document.getElementById("submitButton").onclick = function fun() {
-        var firstName = document.getElementById("first").value;
-        var lastName = document.getElementById("last").value;
-        var email = document.getElementById("email").value;
+        var firstName = document.getElementById("first").val();
+        var lastName = document.getElementById("last").val();
+        var email = document.getElementById("email").val();
+        user.set("firstName", firstName)
+        user.set("lastName", lastName)
+        user.set("email", email)
         alert(firstName + lastName + email)
+        
+        user.save(null,{
+            success: function(user){
+                alert("Submittion complete. We look forward to seeing you soon")
+            },
+            error: function(user, error){
+                alert("Failed to submit, error code: " +error.message)
+            }
+        });
         //validation code to see State field is mandatory.  
     }
 }
