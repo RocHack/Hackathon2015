1ad61209a38c921f631ed9c702fbbccfee7868b0
PromiscuousFish <jacob.bisnett@gmail.com>
2015-02-13 17:52:40 -0500

    Update submitForm.js

--- javascripts/submitForm.js
+++ javascripts/submitForm.js
@@ -12,17 +12,16 @@ window.onload = function() {
         var firstName = document.getElementById("first").val();
         var lastName = document.getElementById("last").val();
         var email = document.getElementById("email").val();
-        user.set("firstName", firstName)
-        user.set("lastName", lastName)
-        user.set("email", email)
-        alert(firstName + lastName + email)
+        user.set("firstName", firstName);
+        user.set("lastName", lastName);
+        user.set("email", email);
         
         user.save(null,{
             success: function(user){
-                alert("Submittion complete. We look forward to seeing you soon")
+                alert("Submittion complete. We look forward to seeing you soon.");
             },
             error: function(user, error){
-                alert("Failed to submit, error code: " +error.message)
+                alert("Failed to submit, error code: " +error.message);
             }
         });
         //validation code to see State field is mandatory.  
