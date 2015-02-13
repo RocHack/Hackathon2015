47641769667e8db64234a8987bd88d1b84b76f09
Colin Pronovost <colin.pron@live.com>
2015-02-13 17:42:22 -0500

    Fix for jQuery val()

--- javascripts/submitForm.js
+++ javascripts/submitForm.js
@@ -8,9 +8,9 @@ testObject.save({foo: "bar"}).then(function(object) {
 
 window.onload = function() {
     document.getElementById("submitButton").onclick = function fun() {
-        var firstName = document.getElementById("first").value;
-        var lastName = document.getElementById("last").value;
-        var email = document.getElementById("email").value;
+        var firstName = document.getElementById("first").val();
+        var lastName = document.getElementById("last").val();
+        var email = document.getElementById("email").val();
         alert(firstName + lastName + email)
         //validation code to see State field is mandatory.  
     }
